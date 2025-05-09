fc8d2613e03b2659520cc05f5e8cfa1af07d8238
Kepler Sticka-Jones <kepler@stickajones.org>
2025-05-09 13:40:42 -0600

    blesh: Integrate with starship prompt

--- private_dot_config/blesh/init.sh
+++ private_dot_config/blesh/init.sh
@@ -5,6 +5,9 @@ if [ -x "$(command -v starship)" ]; then
   # Out of the box starship will color the prompt depending on the previous exit code
   # Additionally, with the status module enabled starship increases exit code verbosity
   bleopt exec_errexit_mark=''
+  # Integrate starship with TransientPrompt
+  # Ref: https://starship.rs/advanced-config/#transientprompt-and-transientrightprompt-in-bash
+  bleopt prompt_ps1_final='$(starship module character)'
 fi
 
 # Disable the exit output
